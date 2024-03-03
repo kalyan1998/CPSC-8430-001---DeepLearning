a506f80ca3f0c9b92237431d212cf6726cfae189
kalyan1998 <31040112+kalyan1998@users.noreply.github.com>
2024-03-02 19:59:16 -0500

    Update hw2_seq2seq.sh

--- hw2/hw2_1/hw2_seq2seq.sh
+++ hw2/hw2_1/hw2_seq2seq.sh
@@ -1,3 +1,4 @@
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1mARNBxX3SCoCj1YkdeQa84KV_iR41A45' -O Shiva_HW2_model0.h5
 python3 train_and_test.py $1 $2
 # 1) If you want to train the model you can run the same script providing input as shown below
 # sh hw2_seq2seq.sh /MLDS_hw2_1_data/training_data training_label.json
